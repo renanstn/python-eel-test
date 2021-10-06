e535de90866ab05eeb93001dc177114dbd357893
Renan Santana <renan.santana@instruct.com.br>
2021-10-06 08:29:25 -0300

    def send_data function

--- src/functions.py
+++ src/functions.py
@@ -1,8 +1,36 @@
-import eel
+import base64
+import json
 import requests
+import eel
 
 
 @eel.expose
-def send_data(url: str, table: str, query: str, fields: str) -> dict:
-    # TODO
-    pass
+def send_data(url: str, table: str, query: str, fields: str) -> list:
+    request_url = f"{url}/snow/genericQuery"
+    fields_list = fields.split('\n')
+    request_fields = ",".join(fields_list)
+    request_headers = {
+        'Content-Type': 'application/json',
+    }
+    request_payload = {
+        'table': table,
+        'sysparam_fields': request_fields,
+        'sysparam_display_value': True,
+        'sysparam_exclude_reference_link': True,
+        'sysparam_limit': 100,
+        'sysparam_offset': 0,
+        'sysparam_query': query
+    }
+    request_data = json.dumps(request_payload)
+    response = requests.post(
+        request_url,
+        headers=request_headers,
+        data=request_data,
+        verify=False
+    )
+    results = response.json().get('result', [])
+    if results:
+        results = base64.b64decode(results)
+        result_str = results.decode("utf-8")
+        return json.loads(f'[{result_str}]')
+    return []
