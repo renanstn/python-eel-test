534cc640fe0cc57bcd2ab81586bdaa3c42445c7f
Renan Santana <renan_ota@hotmail.com>
2022-11-07 23:32:22 -0300

    Add loading icon and copy to clipboard button

--- src/web/js/script.js
+++ src/web/js/script.js
@@ -1,48 +1,59 @@
 function send_data(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const tibcoUrl = document.getElementById("tibco_url").value;
-    const tableName = document.getElementById("table").value;
-    const query = document.getElementById("query").value;
-    const fields = document.getElementById("fields").value;
+  document.getElementById("content").style.display = "none";
+  document.getElementById("loading-icon").style.display = "block";
 
-    eel.send_data(tibcoUrl, tableName, query, fields)(function(response) {
-        const string_response = JSON.stringify(response);
-        document.getElementById("response-text").value = string_response;
-        change_tab_to_response();
-    });
+  const tibcoUrl = document.getElementById("tibco_url").value;
+  const tableName = document.getElementById("table").value;
+  const query = document.getElementById("query").value;
+  const fields = document.getElementById("fields").value;
+
+  eel.send_data(tibcoUrl, tableName, query, fields)(function(response) {
+    const string_response = JSON.stringify(response);
+    document.getElementById("response-text").value = string_response;
+    change_tab_to_response();
+    document.getElementById("loading-icon").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  });
 };
 
 function change_tab_to_response() {
-    document.getElementById("response-tab").click();
+  document.getElementById("response-tab").click();
 };
 
 function save_data(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const data = {
-        tibcoUrl: document.getElementById("tibco_url").value,
-        tableName: document.getElementById("table").value,
-        query: document.getElementById("query").value,
-        fields: document.getElementById("fields").value,
-    };
+  const data = {
+    tibcoUrl: document.getElementById("tibco_url").value,
+    tableName: document.getElementById("table").value,
+    query: document.getElementById("query").value,
+    fields: document.getElementById("fields").value,
+  };
 
-    eel.save_data(data);
+  eel.save_data(data);
 };
 
 function load_data() {
-    eel.load_data()(function(data) {
-        document.getElementById("tibco_url").value = data.tibcoUrl;
-        document.getElementById("table").value = data.tableName;
-        document.getElementById("query").value = data.query;
-        document.getElementById("fields").value = data.fields;
-    });
+  eel.load_data()(function(data) {
+    document.getElementById("tibco_url").value = data.tibcoUrl;
+    document.getElementById("table").value = data.tableName;
+    document.getElementById("query").value = data.query;
+    document.getElementById("fields").value = data.fields;
+  });
 };
+
+function copy_data() {
+  const content = document.getElementById("response-text").value;
+  navigator.clipboard.writeText(content);
+}
 
 document.getElementById("send").addEventListener("click", send_data);
 document.getElementById("tibco_url").addEventListener("change", save_data);
 document.getElementById("table").addEventListener("change", save_data);
 document.getElementById("query").addEventListener("change", save_data);
 document.getElementById("fields").addEventListener("change", save_data);
+document.getElementById("copy").addEventListener("click", copy_data);
 
 load_data();
